facac360576de7936ea1e2ff6ecf8af362b13edd
Landon <landoncummings@gmail.com>
2025-02-18 10:12:49 -0500

    secret fix

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAyg-9y4lxXw99tVgHCCzVG3xy3H9K172M",
-  authDomain: "voxa-867e6.firebaseapp.com",
-  projectId: "voxa-867e6",
-  storageBucket: "voxa-867e6.appspot.com",
-  messagingSenderId: "629814098214",
-  appId: "1:629814098214:web:4a04ced847ed53bfa7e9e7",
-  measurementId: "G-EQ43ZTZ687"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
